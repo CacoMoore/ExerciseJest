ed1e5cee0c1643c18ee4f32e2b7b28ba1c6d5b94
CacoMoore <caroavello.m@gmail.com>
2023-02-21 22:00:36 +0000

    exercise jest

---
+++ app.js
@@ -0,0 +1,29 @@
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+function fromDollarToYen(num){
+    return num * 106.6
+}
+
+function fromEuroToDollar(num){
+    return num * 1.2
+}
+
+function fromYenToPound(num){
+    return num * 159.9
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound};
